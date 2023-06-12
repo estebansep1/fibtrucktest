96496a07dbaaa31685e15d611509b3bbfb60f524
Esteban Sepulveda <tebansd@icloud.com>
2023-06-12 14:05:04 -0700

    activity from intro course

---
+++ fibonacci.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    console.log("document is ready")
+  });
+  
+  
+  const navPic =$(".nav-pic");
+  navPic.fadeIn(3000);
+  
+  const aboutUs =$(".about-us");
+  aboutUs.fadeIn(3000);
+  
+  const qtrMenu =$("quarter-menu");
+  qtrMenu.fadeIn(3000);
+    
+  const mathFavs =$("math-favs");
+  mathFavs.fadeIn(3000);
+  
+  const hours =$("hours");
+  hours.fadeIn(3000);
+  
+  const locationInfo =$("loaction-info");
+  locationInfo.fadeIn(3000);
